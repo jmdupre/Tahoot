36955c7a69a7a7bd6e39a93cede450e2314402a7
Nana Kwame <49786086+PapaGede@users.noreply.github.com>
2020-12-10 15:44:23 +0000

    First Commit

---
+++ tahoot/src/main/java/com/turntabl/io/tahoot/controller/AnswerController.java
@@ -0,0 +1,23 @@
+package com.turntabl.io.tahoot.controller;
+
+
+import com.turntabl.io.tahoot.model.Answers;
+import com.turntabl.io.tahoot.model.Questions;
+import com.turntabl.io.tahoot.repository.AnswersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnswerController {
+
+    @Autowired
+    AnswersRepository answersRepository;
+
+        @PostMapping("/create_answer")
+    public String questions(@RequestBody Answers answers){
+        answersRepository.save(answers);
+        return "Answers saved";
+    }
+}
